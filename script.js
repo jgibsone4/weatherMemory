17a649c1b03a99b55d5ad27415c1df7e79524592
Jason Gibson <71955479+jgibsone4@users.noreply.github.com>
2021-01-11 21:47:33 -0500

    Update script.js
    Fixed a lot

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-//INPUT VARgit
 
-var startDateInput;
-var endDateInput;
-var startTimeInput;
-var endTimeInput;
-var birthLocationInput;
+$(document).ready(function () {
 
-var startDateInput = document.querySelector(".startDateInput");
+//Create value from input
+var cityInput = $('.cityInput').val();
+var stateInput = $('.stateInput').val();
+var monthInput = $('.monthInput').val();
+var yearInput = $('.yearInput').val();
+var hourInput = $('.hourInput').val();
+var minuteInput = $('.minuteInput').val();
+var dayInput = $('.dayInput').val();
+
 
 //https://www.youtube.com/watch?v=BvU4L2C1wcI
 
-$(document).ready(function () {
-  $(".form-submit").submit(function (event) {
-    //event listener
-    performSearch(event);
-  });
-
-  function performSearch(event) {
+     function performSearch(event) {
     event.preventDefault();
 
-    var apikey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
+    var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
     var queryURL =
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=1975-04-22T00:00:00&endDateTime=1975-04-22T01:00:00&unitGroup=us&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location=Sterling,VA,US&key=" +
-      apikey;
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&endDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":30&unitGroup=us&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location="+cityInput+","+stateInput+",US&key="+apiKey;
 
     $.ajax({
       url: queryURL,
@@ -30,19 +26,68 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
     });
+   
+
+        
 
     function formatSearch(jsonObject) {
       // rename and correct display results
-      var startDateInput = jsonObject.startDateInput;
+      /* var startDateInput = jsonObject.startDateInput;
       var endDateInput = jsonObject.endDateInput;
       var startTimeInput = jsonObject.startTimeInput;
       var endTimeInput = jsonObject.endTimeInput;
-      var birthLocationInput = jsonObject.birthLocationInput;
+      var birthLocationInput = jsonObject.birthLocationInput; */
 
       //What is displayed
       $(".city-name").text(birthLocationInput); //??Change to output??
       $(".display-time").text(startTimeInput);
       ".historical-weather".text(historicalweatherOutput); //check reuslts in api
     }
+
+
   }
+   //Call the function
+ $(".submitBtn").click(performSearch);
 });
+
+
+
+// //INPUT VAR
+// var DateInput;
+// var TimeInput;
+// var birthLocationInput;
+// var search;
+
+// $(document).ready(function (){
+//     $('.search').submit(function(event){ //event listener
+//         performSearch(event);
+//     });
+//     });
+//     $('.search').click(function(){
+//         $.getJSON(queryURL, function( cityTempData ) {
+//           // cityTempData is the json object
+//           console.log('2) output the api data here', cityTempData);
+//           $('#city-name').text(cityTempData.name);
+//           console.log('show me main object', cityTempData.main);
+//           $('#current-temp').text(cityTempData.main.temp);
+//         });
+//         function performSearch(event){
+//             event.preventDefault();
+//             var key = "G9CVLC87MQTZ38PJ3ZJZCWRZJ"
+//             var queryURL =
+//             "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=1975-04-22T00:00:00&endDateTime=1975-04-22T01:00:00&unitGroup=us&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location=" + birthLocationInput.value + key
+//             $.ajax({
+//               url: queryURL,
+//               method: "GET",
+//             }).then(function (response) {
+//               console.log(response);
+//             });
+
+//     function formatSearch(jsonObject) { // rename and correct display results
+//         var startDateInput = jsonObject.name;
+//         var endDateInput = jsonObject.end;        
+//         var startTimeInput = jsonObject.end;  
+//         var endTimeInput = jsonObject.end;  
+//         var birthLocationInput = jsonObject.end;  
+//     }
+//     }})
