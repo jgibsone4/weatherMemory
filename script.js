dda108b935a924142154bcbe4e38cad7fb65d0ae
Jason Gibson <71955479+jgibsone4@users.noreply.github.com>
2021-01-13 16:53:52 -0500

    Update script.js
    with NASA API

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-
 $(document).ready(function () {
+  //Create value from input
+  var cityInput = $(".cityInput");
+  var stateInput = $(".stateInput");
+  var monthInput = $(".monthInput");
+  var yearInput = $(".yearInput");
+  var hourInput = $(".hourInput");
+  var minuteInput = $(".minuteInput");
+  var dayInput = $(".dayInput");
 
-//Create value from input
-var cityInput = $('.cityInput');
-var stateInput = $('.stateInput');
-var monthInput = $('.monthInput');
-var yearInput = $('.yearInput');
-var hourInput = $('.hourInput');
-var minuteInput = $('.minuteInput')
-var dayInput = $('.dayInput');
-
-
-
-
-
-
-     function performSearch(event) {
-
+  function performSearch(event) {
     event.preventDefault();
 
     console.log(yearInput.val());
     console.log(dayInput.val());
 
     var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
-   /*  var queryURL =
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&endDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":30&unitGroup=us&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location="+cityInput+","+stateInput+",US&key="+apiKey; */
+
+    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fredericksburg%2C%20VA%2C%20US?unitGroup=us&key=U6CNYSFK8K66GHL8X59APHAQF&include=obs%2Cfcst%2Cstats%2Chours%2Calerts%2Ccurrent%2Cevents="+apiKey;
     var startDate = `${yearInput.val()}-${monthInput.val()}-${dayInput.val()}`;
-    console.log(startDate);
-    var endDate = "";
-
-
-    var queryURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/richmond%2Cva/${startDate}/${endDate}?unitGroup=us&key=${apiKey}`
+  
+    var endDate = 
+      "${yearInput.val()}-${monthInput.val()}-${dayInput.val()}-${hourInput.val()}-${minuteInput.val()}:30-"; // error in console
+      console.log(startDate);
+console.log(endDate);
+    
 
     $.ajax({
       url: queryURL,
@@ -39,29 +32,30 @@ var dayInput = $('.dayInput');
       console.log(response);
       formatSearch(response);
     });
-   
-
-        
+    var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
+    var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      
+    });
 
     function formatSearch(jsonObject) {
       // rename and correct display results
       /* var startDateInput = jsonObject.startDateInput;
-      var endDateInput = jsonObject.endDateInput;
-      var startTimeInput = jsonObject.startTimeInput;
-      var endTimeInput = jsonObject.endTimeInput;
-      var birthLocationInput = jsonObject.birthLocationInput; */
+        var endDateInput = jsonObject.endDateInput;
+        var startTimeInput = jsonObject.startTimeInput;
+        var endTimeInput = jsonObject.endTimeInput;
+        var birthLocationInput = jsonObject.birthLocationInput; */
 
       //What is displayed
-      $("#city-name").text(cityInput.val()); 
-     /*  $(".stateInput").text(stateInput.val());
-      $(".historical-weather").text(historicalweatherOutput);  */
+      $("#city-name").text(cityInput.val());
+      /*  $(".stateInput").text(stateInput.val());
+        $(".historical-weather").text(historicalweatherOutput);  */
     }
-
-   
-
   }
-   //Call the function
- $(".submitBtn").click(performSearch);
+  //Call the function
+  $(".submitBtn").click(performSearch);
 });
-
-
