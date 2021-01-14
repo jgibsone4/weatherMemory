528ec26699c99d6887acfb1a28fa5ee24b9ebae7
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-01-13 20:51:05 -0500

    updating JS

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 $(document).ready(function () {
   //Create value from input
-  var cityInput = $(".cityInput");
-  var stateInput = $(".stateInput");
-  var monthInput = $(".monthInput");
-  var yearInput = $(".yearInput");
-  var hourInput = $(".hourInput");
-  var minuteInput = $(".minuteInput");
-  var dayInput = $(".dayInput");
+  // var cityInput = $(".cityInput");
+  // var stateInput = $(".stateInput");
+  // var monthInput = $(".monthInput");
+  // var yearInput = $(".yearInput");
+  // var hourInput = $(".hourInput");
+  // var minuteInput = $(".minuteInput");
+  // var dayInput = $(".dayInput");
+
+var cityInput = "Richmond";
+var stateInput = "VA";
+var yearInput = "1985";
+var monthInput = "02";
+var dayInput = "13";
+var hourInput = "09";
+var minuteInput = "25";
+
+
 
   function performSearch(event) {
     event.preventDefault();
 
-    console.log(yearInput.val());
-    console.log(dayInput.val());
+    // console.log(yearInput.val());
+    // console.log(dayInput.val());
 
     var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
 
-    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fredericksburg%2C%20VA%2C%20US?unitGroup=us&key=U6CNYSFK8K66GHL8X59APHAQF&include=obs%2Cfcst%2Cstats%2Chours%2Calerts%2Ccurrent%2Cevents="+apiKey;
-    var startDate = `${yearInput.val()}-${monthInput.val()}-${dayInput.val()}`;
+    var queryURL =   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&endDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&unitGroup=us&contentType=json&dayStartTime=0:0:00&dayEndTime=0:0:00&location="+cityInput+","+stateInput+",US&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ"
+    
+    // "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?aggregateHours=24&locationMode=single&contentType=json&unitGroup=us&key="+apiKey+"&locations=New York City,NY"
+
+    // "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fredericksburg%2C%20VA%2C%20US?unitGroup=us&key=U6CNYSFK8K66GHL8X59APHAQF&include=obs%2Cfcst%2Cstats%2Chours%2Calerts%2Ccurrent%2Cevents="+apiKey;
+    // var startDate = `${yearInput.val()}-${monthInput.val()}-${dayInput.val()}`;
   
-    var endDate = 
-      "${yearInput.val()}-${monthInput.val()}-${dayInput.val()}-${hourInput.val()}-${minuteInput.val()}:30-"; // error in console
-      console.log(startDate);
-console.log(endDate);
+//     var endDate = 
+//       "${yearInput.val()}-${monthInput.val()}-${dayInput.val()}-${hourInput.val()}-${minuteInput.val()}:30-"; // error in console
+//       console.log(startDate);
+// console.log(endDate);
     
 
     $.ajax({
@@ -32,15 +46,16 @@ console.log(endDate);
       console.log(response);
       formatSearch(response);
     });
-    var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
-    var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
+
+    // var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
+    // var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET",
+    // }).then(function (response) {
+    //   console.log(response);
       
-    });
+    // });
 
     function formatSearch(jsonObject) {
       // rename and correct display results
