aa4e2608ff14ffd8365c1155a31924a6317091a6
Amanda <awiehrs.photo@gmail.com>
2021-01-14 21:27:50 -0500

    testing

--- script.js
+++ script.js
@@ -1,61 +1,67 @@
-$(document).ready(function () {
-  //Create value from input
-  var cityInput = $(".cityInput");
-  var stateInput = $(".stateInput");
-  var monthInput = $(".monthInput");
-  var yearInput = $(".yearInput");
-  var hourInput = $(".hourInput");
-  var minuteInput = $(".minuteInput");
-  var dayInput = $(".dayInput");
+function selectValue(){
+  var value = $("#monthInput").val();
+  $(".monthResult").text(value);
+  return value
+};  
+  
+  $('select').formSelect();
+
+  var cityInput = "Fredericksburg";
+  var stateInput = "VA";
+  var yearInput = "1985";
+  var monthInput = $("#monthInput").on("change", selectValue);
+  var mTest = parseInt($("#monthInput").val(), 10);
+  var dayInput = "13";
+  //var hourInput = "09";
+  //var minuteInput = "25";
+
+  //$("#monthInput").on("change",function(){
+    //Getting Value
+    //var monthInput = $("#monthInput").val();
+    //$(".monthResult").text(monthInput);
+  //});
+
 
   function performSearch(event) {
     event.preventDefault();
-
-    console.log(yearInput.val());
-    console.log(dayInput.val());
-
-    var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
-
-    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fredericksburg%2C%20VA%2C%20US?unitGroup=us&key=U6CNYSFK8K66GHL8X59APHAQF&include=obs%2Cfcst%2Cstats%2Chours%2Calerts%2Ccurrent%2Cevents="+apiKey;
-    var startDate = `${yearInput.val()}-${monthInput.val()}-${dayInput.val()}`;
-  
-    var endDate = 
-      "${yearInput.val()}-${monthInput.val()}-${dayInput.val()}-${hourInput.val()}-${minuteInput.val()}:30-"; // error in console
-      console.log(startDate);
-console.log(endDate);
     
+    //var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
 
+    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+cityInput+"%2C%20"+stateInput+"%2C%20US/"+yearInput+"-"+monthInput+"-"+dayInput+"/"+yearInput+"-"+monthInput+"-"+dayInput+"?unitGroup=us&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ&include=obs%2Cfcst"
+
+    //var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+cityInput+"%2C%20"+stateInput+"%2C%20US/"+yearInput+"-"+monthInput+"-"+dayInput+"/"+yearInput+"-"+monthInput+"-"+dayInput+"?unitGroup=us&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ&include=obs%2Cfcst"
+
+    
+    //"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&endDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&unitGroup=us&contentType=json&dayStartTime=0:0:00&dayEndTime=0:0:00&location="+cityInput+","+stateInput+",US&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ"
+    console.log(monthInput);
+    
+  
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
+      //var weatherCon = response.locations[cityInput+","+stateInput+",US"].values[0].conditions;
       console.log(response);
-      formatSearch(response);
-    });
-    var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
-    var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
+      //console.log(weatherCon);
+      
+      
       
     });
 
-    function formatSearch(jsonObject) {
-      // rename and correct display results
-      /* var startDateInput = jsonObject.startDateInput;
-        var endDateInput = jsonObject.endDateInput;
-        var startTimeInput = jsonObject.startTimeInput;
-        var endTimeInput = jsonObject.endTimeInput;
-        var birthLocationInput = jsonObject.birthLocationInput; */
+    
 
-      //What is displayed
-      $("#city-name").text(cityInput.val());
-      /*  $(".stateInput").text(stateInput.val());
-        $(".historical-weather").text(historicalweatherOutput);  */
-    }
-  }
+    // var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
+    // var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET",
+    // }).then(function (response) {
+    //   console.log(response);
+      
+    // });
+  };
+
+
   //Call the function
   $(".submitBtn").click(performSearch);
-});
+
