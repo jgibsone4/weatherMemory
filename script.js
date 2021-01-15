5c4e402baaa23e4e25e5e1b4e2c8e74362e864b6
Amanda Wiehrs <71985332+awiehrs@users.noreply.github.com>
2021-01-15 02:18:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,47 @@
 
-function selectValue(){
-  var value = $("#monthInput").val();
-  $(".monthResult").text(value);
-  return value
-};  
-  
+$(document).ready(function() {
+  $(".resultsPage").hide();
+
   $('select').formSelect();
-
-  var cityInput = "Fredericksburg";
-  var stateInput = "VA";
-  var yearInput = "1985";
-  var monthInput = $("#monthInput").on("change", selectValue);
-  var mTest = parseInt($("#monthInput").val(), 10);
-  var dayInput = "13";
-  //var hourInput = "09";
-  //var minuteInput = "25";
-
-  //$("#monthInput").on("change",function(){
-    //Getting Value
-    //var monthInput = $("#monthInput").val();
-    //$(".monthResult").text(monthInput);
-  //});
-
+  
+  function GetInputValue(field){
+    var sel = document.getElementById(field)
+    return sel.options[sel.selectedIndex].value
+  }
 
   function performSearch(event) {
     event.preventDefault();
-    
-    //var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
 
-    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+cityInput+"%2C%20"+stateInput+"%2C%20US/"+yearInput+"-"+monthInput+"-"+dayInput+"/"+yearInput+"-"+monthInput+"-"+dayInput+"?unitGroup=us&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ&include=obs%2Cfcst"
+    monthValue=GetInputValue('monthInput');
+    dayInput = GetInputValue('dayInput');
+    var yearInput = $("#yearInput").val();
+    var cityInput = $("#cityInput").val();
+    var stateInput = $("#stateInput").val();
+   
+    var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
 
-    //var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+cityInput+"%2C%20"+stateInput+"%2C%20US/"+yearInput+"-"+monthInput+"-"+dayInput+"/"+yearInput+"-"+monthInput+"-"+dayInput+"?unitGroup=us&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ&include=obs%2Cfcst"
+    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+cityInput+"%2C%20"+stateInput+"%2C%20US/"+yearInput+"-"+monthValue+"-"+dayInput+"/"+yearInput+"-"+monthValue+"-"+dayInput+"?unitGroup=us&key="+apiKey+"&include=obs%2Cfcst%2Cstats%2Calerts"
 
-    
-    //"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&endDateTime="+yearInput+"-"+monthInput+"-"+dayInput+"T"+hourInput+":"+minuteInput+":00&unitGroup=us&contentType=json&dayStartTime=0:0:00&dayEndTime=0:0:00&location="+cityInput+","+stateInput+",US&key=G9CVLC87MQTZ38PJ3ZJZCWRZJ"
-    console.log(monthInput);
-    
   
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      //var weatherCon = response.locations[cityInput+","+stateInput+",US"].values[0].conditions;
+      var conditions = response.days[0].conditions;
+      var precip = response.days[0].precip;
+      var temp = response.days[0].temp;
+      var snowdepth = response.days[0].snowdepth;
+      var address = response.days[0].address;
       console.log(response);
-      //console.log(weatherCon);
-      
-      
-      
+      $("#conditionsDisplay").text(conditions);
     });
 
-    
+    $("#cityName").text(cityInput);
+    $("#stateName").text(stateInput);
+    $("#monthDisplay").text(monthValue);
+    $("#dayDisplay").text(dayInput);
+    $("#yearDisplay").text(yearInput);
+
 
     // var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
     // var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
@@ -62,7 +54,25 @@ function selectValue(){
     // });
   };
 
+  
+
 
   //Call the function
   $(".submitBtn").click(performSearch);
+
+  //Button Event Listeners - Toggles between Search and Results Page
+  $(".submitBtn").click(function() {
+    $(".frontPage").fadeOut(400, function () {
+            $('.resultsPage').fadeIn();
+        });
+  });
+
+  $(".homeBtn").click(function() {
+    $(".resultsPage").fadeOut(400, function () {
+      $('.frontPage').fadeIn();
+  });
+});
+
+});
+
 
