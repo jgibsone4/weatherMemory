14b552dae64e7da8c6af8a024f1ebc91916da0f3
Kenneth Keller <71411829+kenkanifffromct@users.noreply.github.com>
2021-01-16 02:35:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var $monthInput = $("#monthInput");
-var $dayInput = $("#dayInput");
-var $yearInput = $("#yearInput");
-var $cityInput = $("#cityInput");
-var $stateInput = $("#stateInput");
-
 $(document).ready(function() {
   $(".resultsPage").hide();
   $('select').formSelect();
@@ -38,7 +32,14 @@ $(document).ready(function() {
     $("#monthDisplay").text(monthValue);
     $("#dayDisplay").text(dayInput);
     $("#yearDisplay").text(yearInput);
-
+    // var apiKey  = "gs7YZMi9EDZOcxQZ3oiMyOokV9g4S74u4MiKtOI9";
+    // var queryURL ="https://api.nasa.gov/planetary/apod?api_key="+apiKey;
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET",
+    // }).then(function (response) {
+    //   console.log(response);
+    // });
   };
   //Call the function
   $(".submitBtn").click(performSearch);
@@ -56,7 +57,7 @@ $(document).ready(function() {
           "url": "https://billboard-api2.p.rapidapi.com/hot-100?date="+year+"-"+month+"-"+day+"&range=1-10",
           "method": "GET",
           "headers": {
-            "x-rapidapi-key": "1aed5862e1mshd7e0b92aad20dadp15dcf4jsn5123a112cbee",
+            "x-rapidapi-key": "0f02775473mshc14b0d816c62eb9p13fc46jsnbc8853f8dafb",
             "x-rapidapi-host": "billboard-api2.p.rapidapi.com"
           }
         };
@@ -64,28 +65,13 @@ $(document).ready(function() {
           console.log(response.content[1].artist);
           var songNumberOne = response.content[1].title;
           var artistNumberOne = response.content[1].artist;
-          $(".songResult").text(artistNumberOne)
+          $(".songResult").text(artistNumberOne);
+          $(".songResult").append(" - "+ '"'+songNumberOne+'"');
         });
   });
   $(".homeBtn").click(function() {
     $(".resultsPage").fadeOut(400, function () {
       $('.frontPage').fadeIn();
-      localStorage.setItem("month", $monthInput.val());
-            localStorage.setItem("day", $dayInput.val());
-            localStorage.setItem("year", $yearInput.val());
-            localStorage.setItem("city", $cityInput.val());
-            localStorage.setItem("state", $stateInput.val());
-            //event persists when saved
- 
-            $("#monthInput").append(localStorage.getItem("month"));
-            $("#dayInput").append(localStorage.getItem("day"));
-            $("#yearInput").append(localStorage.getItem("year"));
-            $("#cityInput").append(localStorage.getItem("city"));
-            $("#stateInput").append(localStorage.getItem("state"));
   });
 });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 3d7ea1155f2cc8c09c330838e5aeafb9d8a9355b
