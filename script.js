8a6ceab384d8c4bfb933e3df24842028ee674edc
Kenneth Keller <71411829+kenkanifffromct@users.noreply.github.com>
2021-01-15 19:35:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,98 @@
-//INPUT VARgit
+$(document).ready(function() {
+  $(".resultsPage").hide();
 
-var startDateInput;
-var endDateInput;
-var startTimeInput;
-var endTimeInput;
-var birthLocationInput;
-
-var startDateInput = document.querySelector(".startDateInput");
-
-//https://www.youtube.com/watch?v=BvU4L2C1wcI
-
-$(document).ready(function () {
-  $(".form-submit").submit(function (event) {
-    //event listener
-    performSearch(event);
-  });
+  $('select').formSelect();
+  
+  function GetInputValue(field){
+    var sel = document.getElementById(field)
+    return sel.options[sel.selectedIndex].value
+  }
 
   function performSearch(event) {
     event.preventDefault();
 
-    var apikey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
-    var queryURL =
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=1975-04-22T00:00:00&endDateTime=1975-04-22T01:00:00&unitGroup=us&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location=Sterling,VA,US&key=" +
-      apikey;
+    monthValue=GetInputValue('monthInput');
+    dayInput = GetInputValue('dayInput');
+    var yearInput = $("#yearInput").val();
+    var cityInput = $("#cityInput").val();
+    var stateInput = $("#stateInput").val();
+   
+    var apiKey = "G9CVLC87MQTZ38PJ3ZJZCWRZJ";
 
+    var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+cityInput+"%2C%20"+stateInput+"%2C%20US/"+yearInput+"-"+monthValue+"-"+dayInput+"/"+yearInput+"-"+monthValue+"-"+dayInput+"?unitGroup=us&key="+apiKey+"&include=obs%2Cfcst%2Cstats%2Calerts"
+
+  
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
+      var conditions = response.days[0].conditions;
+      var precip = response.days[0].precip;
+      var temp = response.days[0].temp;
+      var snowdepth = response.days[0].snowdepth;
+      var address = response.days[0].address;
       console.log(response);
+      $("#conditionsDisplay").text(conditions);
+      $("#tempDisplay").text(temp);
     });
 
-    function formatSearch(jsonObject) {
-      // rename and correct display results
-      var startDateInput = jsonObject.startDateInput;
-      var endDateInput = jsonObject.endDateInput;
-      var startTimeInput = jsonObject.startTimeInput;
-      var endTimeInput = jsonObject.endTimeInput;
-      var birthLocationInput = jsonObject.birthLocationInput;
+    $("#cityName").text(cityInput);
+    $("#stateName").text(stateInput);
+    $("#monthDisplay").text(monthValue);
+    $("#dayDisplay").text(dayInput);
+    $("#yearDisplay").text(yearInput);
 
-      //What is displayed
-      $(".city-name").text(birthLocationInput); //??Change to output??
-      $(".display-time").text(startTimeInput);
-      ".historical-weather".text(historicalweatherOutput); //check reuslts in api
-    }
+
+    // apiKey2 = "YreeZrO5piLWfH99ELO1qsiST1iuNaBz1sMlVyUc"
+    
+    // var queryUrl2 = "https://api.nasa.gov/planetary/earth/imagery?lon=-95.33&lat=29.78&date="+yearInput"-"+monthValue"-"+dayInput+"&dim=0.15&"+apiKey2;
+
+    // $.ajax({
+    //   url: queryUrl2,
+    //   method: "GET",
+    // }).then(function (response) {
+    //   $("#earth-day").append(response)
+    // });
+    
+  };
+
+  let search = document.querySelector(".submitBtn")
+
+  search.addEventListener("click", ()=> {
+    console.log("search pressed")
+    sendApiRequest()
+  })
+
+  async function sendApiRequest() {
+    let API_KEY = "YreeZrO5piLWfH99ELO1qsiST1iuNaBz1sMlVyUc"
+    let response = await fetch (`https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=${API_KEY}`);
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+    useApiData(data)
   }
+  
+  function useApiData(data) {
+    document.querySelector("#earth-day").innerHTML = data[0].dscovr_j2000_position.image
+  }
+
+
+  //Call the function
+  $(".submitBtn").click(performSearch);
+
+  //Button Event Listeners - Toggles between Search and Results Page
+  $(".submitBtn").click(function() {
+    $(".frontPage").fadeOut(400, function () {
+            $('.resultsPage').fadeIn();
+        });
+  });
+
+  $(".homeBtn").click(function() {
+    $(".resultsPage").fadeOut(400, function () {
+      $('.frontPage').fadeIn();
+  });
 });
 
-// //INPUT VAR
-// var DateInput;
-// var TimeInput;
-// var birthLocationInput;
-// var search;
+});
 
-// $(document).ready(function (){
-//     $('.search').submit(function(event){ //event listener
-//         performSearch(event);
-//     });
-//     });
-//     $('.search').click(function(){
-//         $.getJSON(queryURL, function( cityTempData ) {
-//           // cityTempData is the json object
-//           console.log('2) output the api data here', cityTempData);
-//           $('#city-name').text(cityTempData.name);
-//           console.log('show me main object', cityTempData.main);
-//           $('#current-temp').text(cityTempData.main.temp);
-//         });
-//         function performSearch(event){
-//             event.preventDefault();
-//             var key = "G9CVLC87MQTZ38PJ3ZJZCWRZJ"
-//             var queryURL =
-//             "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=1975-04-22T00:00:00&endDateTime=1975-04-22T01:00:00&unitGroup=us&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location=" + birthLocationInput.value + key
-//             $.ajax({
-//               url: queryURL,
-//               method: "GET",
-//             }).then(function (response) {
-//               console.log(response);
-//             });
 
-//     function formatSearch(jsonObject) { // rename and correct display results
-//         var startDateInput = jsonObject.name;
-//         var endDateInput = jsonObject.end;        
-//         var startTimeInput = jsonObject.end;  
-//         var endTimeInput = jsonObject.end;  
-//         var birthLocationInput = jsonObject.end;  
-//     }
-//     }})
